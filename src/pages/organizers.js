9a6d17873b62efc89db55fca0b04d42a9d0c19aa
Michael Leners <micleners@gmail.com>
2020-10-10 08:47:12 -0500

    Added styled components and rebass. Added three main pages

--- src/pages/organizers.js
+++ src/pages/organizers.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const Organizers = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Organizers" description="Organizers of our group"/>
+    <h1>Organizers</h1>
+    <p>Organizers of our group.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default Organizers
