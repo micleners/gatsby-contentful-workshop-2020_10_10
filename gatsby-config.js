82be005f5ab16afcc075459b5a1b0554baf416e0
Michael Leners <micleners@gmail.com>
2020-10-10 08:20:03 -0500

    Update metadata before netlify deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Events`,
+    description: `A place to find all the information about our cool events.`,
+    author: `@micleners`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
